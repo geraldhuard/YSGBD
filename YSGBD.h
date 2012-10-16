8cae1a787acbd786766479605a0c33aa15b5f396
GildasA <dancehallsoldat@gmail.com>
2012-10-16 16:38:47 +0300

    Update YSGBD.h

--- YSGBD.h
+++ YSGBD.h
@@ -1,40 +1,32 @@
 //
 //  YSGBD.h
-//  EcoFeed
 //
-//  Created by Gérald HUARD on 08/02/12.
+//  Modified by Gildas A. on 16/10/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import "YObject.h"
-#import "/usr/include/sqlite3.h"
+#import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
-@interface YSGBD : YObject{
+@interface YSGBD : NSObject{
     NSString        *dbpathname;
     NSString        *dbfilename;
     BOOL            sgbd_opened;
     NSString        *sgbd_openedpath;
     sqlite3         *sgbd;
-    
-    
 }
 
 @property (nonatomic) sqlite3 *sgbd;
 
-+(YSGBD*)   shared;
++(id)       shared;
 +(void)     sClose;
 +(BOOL)     sExec:(NSString*)req;
-+(NSArray*) sExecAndGet:(NSString*)req;
++(NSMutableArray*) sExecAndGet:(NSString*)req;
 +(BOOL)     sInsertDict:(NSDictionary*)dict intoTable:(NSString*)table;
 +(BOOL)     sUpdateDict:(NSDictionary*)dict intoTable:(NSString*)table withKeysNames:(NSArray*)keys;
-
 +(BOOL)     sPutWithDict:(NSDictionary*)dict intoTable:(NSString*)table andKeysNames:(NSArray*)keys;
-
 +(BOOL)     sRemoveWithDict:(NSDictionary*)dict intoTable:(NSString*)table andKeysNames:(NSArray*)keys;
-
 +(BOOL)     sEmptyTable:(NSString*)tableName;
-
-
 
 -(BOOL)     rowExistWithDict:(NSDictionary*)dict table:(NSString*)table andKeysNames:(NSArray*)keys;
 -(id)       initWithDBFileName:(NSString*)dbfilename;
@@ -42,15 +34,15 @@
 -(void)     open;
 -(void)     close;
 -(BOOL)     exec:(NSString*)req;
--(NSArray*) execAndGet:(NSString*)req;
+-(NSMutableArray*) execAndGet:(NSString*)req;
 -(BOOL)     insertDict:(NSDictionary*)dict intoTable:(NSString*)table;
+-(BOOL)     replaceDict:(NSDictionary*)dict intoTable:(NSString*)table;
 -(BOOL)     putWithDict:(NSDictionary*)dict intoTable:(NSString*)table andKeysNames:(NSArray*)keys;
 -(BOOL)     updateDict:(NSDictionary*)dict intoTable:(NSString*)table withKeysNames:(NSArray*)keys;
 -(BOOL)     removeWithDict:(NSDictionary*)dict fromTable:(NSString*)table andKeysNames:(NSArray*)keys;
 
 
 -(BOOL)     emptyTable:(NSString*)tablename;
-
 -(void)     logerr;
 
 @end
